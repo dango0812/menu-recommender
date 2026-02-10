5b57c396fbb3af6cbfb6ed966d74b542f81bc04b
dango0812 <ehdrbsla12000@gmail.com>
2026-02-10 15:51:41 +0900

    feat: 공용 컴포넌트 추가
    - Text 컴포넌트 추가
    - Flex 컴포넌트 추가
    - 컴포넌트 스타일 병합을 위한 cn(twMerge) 함수 추가

---
+++ components/base/Flex.tsx
@@ -0,0 +1,60 @@
+import { createElement } from 'react';
+
+import { cn } from '@/lib/tailwind-merge';
+
+interface FlexProps extends React.HTMLAttributes<HTMLElement> {
+  children: React.ReactNode;
+  direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
+  justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+  as?: 'div' | 'section' | 'nav' | 'header' | 'footer' | 'article' | 'aside' | 'form' | 'ul' | 'li' | 'ol';
+}
+
+const directionMap = {
+  row: 'flex-row',
+  column: 'flex-col',
+  'row-reverse': 'flex-row-reverse',
+  'column-reverse': 'flex-col-reverse',
+} as const;
+
+const justifyMap = {
+  'flex-start': 'justify-start',
+  'flex-end': 'justify-end',
+  center: 'justify-center',
+  'space-between': 'justify-between',
+  'space-around': 'justify-around',
+  'space-evenly': 'justify-evenly',
+} as const;
+
+const alignMap = {
+  'flex-start': 'items-start',
+  'flex-end': 'items-end',
+  center: 'items-center',
+  stretch: 'items-stretch',
+  baseline: 'items-baseline',
+} as const;
+
+/**
+ * Flex 컴포넌트
+ * @example
+ * ```tsx
+ * <Flex direction="column" alignItems="center" justifyContent="space-between">
+ *   <Icon />
+ *   <Text>Label</Text>
+ * </Flex>
+ * ```
+ */
+export function Flex({ as = 'div', children, direction, justifyContent, alignItems, className, ...props }: FlexProps) {
+  const directionClass = direction ? directionMap[direction] : undefined;
+  const justifyClass = justifyContent ? justifyMap[justifyContent] : undefined;
+  const alignClass = alignItems ? alignMap[alignItems] : undefined;
+
+  return createElement(
+    as,
+    {
+      className: cn('flex', directionClass, justifyClass, alignClass, className),
+      ...props,
+    },
+    children
+  );
+}
---
+++ lib/tailwind-merge.ts
@@ -0,0 +1,25 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * @param inputs - string, array, object
+ * @returns 병합된 string 을 반환합니다.
+ *
+ * ```
+ * Example used string
+ * Code : cn("text-base", "bg-white");
+ * Return : "text-base bg-white"
+ *
+ * Example used array
+ * Code : cn("text-base", ["bg-white", "rounded-lg"]);
+ * Return : "text-base bg-white rounded-lg"
+ *
+ * Example used object
+ * Code : const isAuthenticated = true;
+ * cn("text-base", ["bg-white, "rounded-lg"], { hidden : !isAuthenticated, block : isAuthenticated });
+ * Return : "text-base bg-white rounded-lg, block"
+ * ```
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
