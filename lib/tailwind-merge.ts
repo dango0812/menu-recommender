70aa5e05a6920bd94ecad0128730f5a1d679fe4a
dango0812 <ehdrbsla12000@gmail.com>
2026-02-10 16:15:23 +0900

    docs: tailwind merge 함수 주석 수정

--- lib/tailwind-merge.ts
+++ lib/tailwind-merge.ts
@@ -5,20 +5,19 @@ import { twMerge } from 'tailwind-merge';
  * @param inputs - string, array, object
  * @returns 병합된 string 을 반환합니다.
  *
- * ```
- * Example used string
+ * @example
  * Code : cn("text-base", "bg-white");
  * Return : "text-base bg-white"
  *
- * Example used array
+ * @example
  * Code : cn("text-base", ["bg-white", "rounded-lg"]);
  * Return : "text-base bg-white rounded-lg"
  *
- * Example used object
+ * @example
  * Code : const isAuthenticated = true;
- * cn("text-base", ["bg-white, "rounded-lg"], { hidden : !isAuthenticated, block : isAuthenticated });
- * Return : "text-base bg-white rounded-lg, block"
- * ```
+ * cn("text-base", ["bg-white", "rounded-lg"], { hidden : !isAuthenticated, block : isAuthenticated });
+ * Return : "text-base bg-white rounded-lg block"
+ *
  */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
