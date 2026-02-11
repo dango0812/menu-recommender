887c46ae0803cb72768e84c6a5ce302c0dc6c1a1
dango0812 <ehdrbsla12000@gmail.com>
2026-02-12 00:58:51 +0900

    feat: Lottie 컴포넌트 추가

--- components/base/index.ts
+++ components/base/index.ts
@@ -1,5 +1,6 @@
 export { Card } from './Card';
 export { Flex } from './Flex';
+export { Lottie } from './Lottie';
 export { NumericSpinner } from './NumericSpinner';
 export { Switch } from './Switch';
 export { default as Tab } from './Tabs';
