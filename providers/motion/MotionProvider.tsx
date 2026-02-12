5a854cbb0d4276946ab043f7fae660f1f28f77c2
dango0812 <ehdrbsla12000@gmail.com>
2026-02-12 20:51:17 +0900

    feat: Framer Motion Provider 추가

---
+++ providers/motion/MotionProvider.tsx
@@ -0,0 +1,9 @@
+import { domMax, LazyMotion } from 'framer-motion';
+import type { PropsWithChildren } from 'react';
+
+/**
+ * Framer Motion Provider
+ */
+export default function MotionProvider({ children }: PropsWithChildren) {
+  return <LazyMotion features={domMax}>{children}</LazyMotion>;
+}
