536701c87c275f73a9cb41e1a52e58460f1f4c19
dango0812 <ehdrbsla12000@gmail.com>
2026-03-12 15:14:47 +0900

    refactor: FormHelperText 접근성 개선

--- components/common/form-helper-text.tsx
+++ components/common/form-helper-text.tsx
@@ -4,7 +4,7 @@ import { Text } from '@/components/ui';
 
 export function FormHelperText({ children }: PropsWithChildren) {
   return (
-    <Text as="p" className="pl-1 text-xs text-error">
+    <Text as="p" role="alert" aria-live="polite" aria-atomic="true" className="pl-1 text-xs text-error">
       {children}
     </Text>
   );
