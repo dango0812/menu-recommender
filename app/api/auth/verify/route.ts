4eb7288dffbf1d3a112d4f4cbab4c55275d4bb96
dango0812 <ehdrbsla12000@gmail.com>
2026-03-12 15:53:29 +0900

    refactor: 이메일 확인 API 개선
    - try catch를 통한 실패 경우 대비

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -4,35 +4,39 @@ import { ROUTES } from '@/constants/routes';
 import prisma from '@/lib/prisma';
 
 export async function GET(req: NextRequest) {
-  const token = req.nextUrl.searchParams.get('token');
+  try {
+    const token = req.nextUrl.searchParams.get('token');
 
-  if (!token) {
-    return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?error=invalid`, req.url));
+    if (!token) {
+      return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?error=invalid`, req.url));
+    }
+
+    const verificationToken = await prisma.emailVerificationToken.findUnique({
+      where: { token },
+    });
+
+    // 토큰 없음
+    if (!verificationToken) {
+      return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?error=invalid`, req.url));
+    }
+
+    // 토큰 만료
+    if (verificationToken.expires_at < new Date()) {
+      await prisma.emailVerificationToken.delete({ where: { token } });
+      return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?error=expired`, req.url));
+    }
+
+    // 이메일 확인 업데이트 및 토큰 삭제 (트랜잭션)
+    await prisma.$transaction([
+      prisma.user.update({
+        where: { id: verificationToken.userId },
+        data: { emailVerified: new Date() },
+      }),
+      prisma.emailVerificationToken.delete({ where: { token } }),
+    ]);
+
+    return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?verified=true`, req.url));
+  } catch (e) {
+    return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?error=server`, req.url));
   }
-
-  const verificationToken = await prisma.emailVerificationToken.findUnique({
-    where: { token },
-  });
-
-  // 토큰 없음
-  if (!verificationToken) {
-    return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?error=invalid`, req.url));
-  }
-
-  // 토큰 만료
-  if (verificationToken.expires_at < new Date()) {
-    await prisma.emailVerificationToken.delete({ where: { token } });
-    return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?error=expired`, req.url));
-  }
-
-  // 이메일 확인 업데이트 및 토큰 삭제 (트랜잭션)
-  await prisma.$transaction([
-    prisma.user.update({
-      where: { id: verificationToken.userId },
-      data: { emailVerified: new Date() },
-    }),
-    prisma.emailVerificationToken.delete({ where: { token } }),
-  ]);
-
-  return NextResponse.redirect(new URL(`${ROUTES.AUTH.SIGN_IN}?verified=true`, req.url));
 }
