d863be056e0d6a708f9bd5b59ec3b7735a2f50ca
dango0812 <ehdrbsla12000@gmail.com>
2026-03-14 04:12:59 +0900

    feat: RHFDropzone 컴포넌트 추가
    - react-hook-form 기반 파일 업로드 컴포넌트
    - Controller와 react-dropzone 조합으로 File 객체를 폼 필드에 저장
    - preview URL 생명주기를 useEffect cleanup으로 자동 관리
    - 문제 발생 시, toast 에러 메시지 표시

--- components/common/react-hook-form/index.ts
+++ components/common/react-hook-form/index.ts
@@ -1,4 +1,5 @@
 export { RHFChipGroup } from './rhf-chip-group';
+export { RHFDropzone } from './rhf-dropzone';
 export { RHFInput } from './rhf-input';
 export { RHFFormProvider } from './rhf-provider';
 export { RHFTextarea } from './rhf-textarea';
