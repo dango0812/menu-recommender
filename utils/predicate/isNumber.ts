363be2695d0aa8ac78f520854b9de8bd98533f11
dango0812 <ehdrbsla12000@gmail.com>
2026-03-14 19:48:43 +0900

    refactor: isNumber 함수 로직 단순화 및 매개변수명 변경
    - isNumber 검사 방식을 typeof로 통일

--- utils/predicate/isNumber.ts
+++ utils/predicate/isNumber.ts
@@ -1,8 +1,8 @@
 /**
  * 전달된 값이 숫자인지 확인하는 함수
  *
- * @param {unknown} x - 숫자인지 확인할 값
- * @returns {x is number} 값이 숫자인 경우 true, 그렇지 않은 경우 false를 반환
+ * @param {unknown} value - 숫자인지 확인할 값
+ * @returns {value is number} 값이 숫자인 경우 true, 그렇지 않은 경우 false를 반환
  *
  * @example
  * const value1 = 123;
@@ -13,6 +13,6 @@
  * console.log(isNumber(value2)); // false
  * console.log(isNumber(value3)); // false
  */
-export function isNumber(x: unknown): x is number {
-  return typeof x === 'number' || x instanceof Number;
+export function isNumber(value: unknown): value is number {
+  return typeof value === 'number';
 }
